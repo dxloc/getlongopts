b608aef90273048d055bce8e81292bfedeb2782d
dxloc <dxl.loc@outlook.com>
2022-12-17 15:42:27 +0700

    modified:   README.md 	new file:   getlongopts.sh

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # getlongopts
-Getlongopts linux shell script
+Getlongopts Linux shell script
+
+# How to use
+The script parse options [-a|--long-a <arg>] [-b|--long-b <arg>] [-c|--long-c <arg>] [-d|--long-d].
+Change them to suite your purpose.
---
+++ getlongopts.sh
@@ -0,0 +1,63 @@
+A=""
+B=""
+C=""
+D=N
+
+usage() {
+	echo "Usage: $1"
+	echo "   -a|--long-a <arg>"
+	echo "   -b|--long-b <arg>"
+	echo "   -c|--long-c <arg>"
+	echo "   -d|--long-d"
+}
+
+read_first_arg() {
+	echo $1
+}
+
+read_first_char() {
+	echo $1 | awk '{ print substr($1, 0, 1) }'
+}
+
+while getopts "-:a:b:c:d" OPT; do
+	# Original post: https://stackoverflow.com/a/28466267/519360
+	# Long option: reformulate OPT and OPTARG
+	if [ "$OPT" = "-" ]; then
+		OPT=$OPTARG                    # Extract long option name
+		shift $((OPTIND-1))            # Remove parsed option from $@ list
+		OPTARG=$(read_first_arg $@)    # Extract long option argument (may be empty)
+		FIRST_CHAR=$(read_first_char $OPTARG)
+		# Remove parsed argument from $@ list if OPTARG is not NULL
+		# and the first character of OPTARG is not "-"
+		if [ "$FIRST_CHAR" != "" ]; then
+			if [ "$FIRST_CHAR" != "-"  ]; then
+				shift 1
+			fi
+		fi
+	fi
+	case "$OPT" in
+		a|long-a)
+			A=$OPTARG
+			;;
+		b|long-b)
+			B=$OPTARG
+			;;
+		c|long-c)
+			C=$OPTARG
+			;;
+		d|long-d)
+			D=Y
+			;;
+		*)
+			usage $0
+			exit
+			;;
+	esac
+done
+shift $((OPTIND-1)) # Remove parsed options and arguments from $@ list
+
+echo "A=$A"
+echo "B=$B"
+echo "C=$C"
+echo "D=$D"
+
